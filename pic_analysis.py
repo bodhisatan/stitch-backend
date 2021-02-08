b9f7183b271e8220be1647d3f2b1ac84b0fb4292
yaoxianjie <yaoxianjie@bytedance.com>
2021-02-08 23:54:57 +0800

    新增计算图片相似度、psnr等接口

---
+++ pic_analysis.py
@@ -0,0 +1,103 @@
+# -*- coding: utf8 -*-
+# author: yaoxianjie
+# date: 2021/2/8
+
+import cv2
+import numpy as np
+import math
+
+# 计算图像直方图相似度、SSIM结构化相似度、PSNR指标
+
+def calculate(image1, image2):
+    # 灰度直方图算法
+    # 计算单通道的直方图的相似值
+    hist1 = cv2.calcHist([image1], [0], None, [256], [0.0, 255.0])
+    hist2 = cv2.calcHist([image2], [0], None, [256], [0.0, 255.0])
+    # 计算直方图的重合度
+    degree = 0
+    for i in range(len(hist1)):
+        if hist1[i] != hist2[i]:
+            degree = degree + \
+                     (1 - abs(hist1[i] - hist2[i]) / max(hist1[i], hist2[i]))
+        else:
+            degree = degree + 1
+    degree = degree / len(hist1)
+    return degree
+
+
+def classify_hist_with_split(image1, image2, size=(256, 256)):
+    # RGB每个通道的直方图相似度
+    # 将图像resize后，分离为RGB三个通道，再计算每个通道的相似值
+    image1 = cv2.resize(image1, size)
+    image2 = cv2.resize(image2, size)
+    sub_image1 = cv2.split(image1)
+    sub_image2 = cv2.split(image2)
+    sub_data = 0
+    for im1, im2 in zip(sub_image1, sub_image2):
+        sub_data += calculate(im1, im2)
+    sub_data = sub_data / 3
+    return sub_data
+
+
+def ssim(img1, img2):
+    C1 = (0.01 * 255) ** 2
+    C2 = (0.03 * 255) ** 2
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1 ** 2
+    mu2_sq = mu2 ** 2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+def calculate_ssim(img1, img2, size=(256, 256)):
+    """calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    """
+    # resize
+    img1 = cv2.resize(img1, size)
+    img2 = cv2.resize(img2, size)
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+
+
+def psnr(target, ref, size=(256, 256)):
+    # resize
+    target = cv2.resize(target, size)
+    ref = cv2.resize(ref, size)
+    # 将图像格式转为float64
+    target_data = np.array(target, dtype=np.float64)
+    ref_data = np.array(ref, dtype=np.float64)
+    # 直接相减，求差值
+    diff = ref_data - target_data
+    # 按第三个通道顺序把三维矩阵拉平
+    diff = diff.flatten('C')
+    # 计算MSE值
+    rmse = math.sqrt(np.mean(diff ** 2.))
+    # 精度
+    eps = np.finfo(np.float64).eps
+    if rmse == 0:
+        rmse = eps
+    return 20 * math.log10(255.0 / rmse)
